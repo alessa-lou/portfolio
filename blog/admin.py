5d2ccfcefc611476b70c8fcb856ed453bd82f37b
alessa-lou <alessa_hardwick@live.co.uk>
2021-03-23 16:17:39 +0000

    added blog views

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from blog.models import Post, Category
+#above line: models you want to register on admin page
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+# last two lines register models with the admin classes
+
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
+from blog.models import Post, Comment
+from .forms import CommentForm
 
-# Create your views here.
+def blog_index(request):
+    posts = Post.objects.all().order_by('-created_on')
+    #^ obtain queryset containing all posts in db
+    # all() is the queryset bit
+    # minus sign in order_by means start with largest value
+    context = {
+        "posts": posts,
+    }
+    return render(request, "blog_index.html", context)
+
+def blog_category(request, category):
+    # next line is django queryset filter - argument tells conditions that need sto be met for an object to be retrieved
+    # so in this case a category with the name corresponding to that in the given argument of the function
+    posts = Post.objects.filter(
+        categories__name__contains=category
+    ).order_by(
+        '-created_on'
+    )
+    context={
+        "category":category,
+        "posts":posts
+    }
+    return render(request, "blog_category.html", context)
+
+def blog_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    # ^ retrieves object with given primary key
+    comments = Comment.objects.filter(post=post)
+    form = CommentForm()
+    if request.method == "POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment=Comment(
+                author=form.cleaned_data["author"],
+                body=form.cleaned_data["body"],
+                post=post
+            )
+            comment.save()
+
+
+    # retrieve all comments assigned to given post using django filters
+    context={
+        "post":post,
+        "comments": comments,
+        "form":form,
+    }
+
+    return render(request, "blog_detail.html", context)
