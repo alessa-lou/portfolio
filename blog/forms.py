3d774858b656126f17060633c12d81ae4b92ddf6
alessa-lou <alessa_hardwick@live.co.uk>
2021-04-01 16:11:05 +0100

    added blog templates

--- blog/forms.py
+++ blog/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 class CommentForm(forms.Form):
     author = forms.CharField(
-        max_length=60
+        max_length=60,
         widget=forms.TextInput(attrs={
             "class":"form-control",
             "placeholder":"Your Name"
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from blog.forms import CommentForm
 from blog.models import Post, Comment
-from .forms import CommentForm
+
 
 def blog_index(request):
     posts = Post.objects.all().order_by('-created_on')
